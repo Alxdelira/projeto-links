74cd514af2c1651fb03bedd49eae41ee1be7c319
Alexandre <alx.delira@gmail.com>
2023-10-20 16:27:36 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains('light')) {
+        img.setAttribute("src", "./assets/lightMode.jpeg")
+    } else {
+        img.setAttribute("src", "./assets/darkMode.jpeg")
+    }
+}
